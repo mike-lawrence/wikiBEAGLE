7d744d5062ea09256d2a09f6f0b2eee3bdc7d2b4
Mike Lawrence <mike.lwrnc@gmail.com>
2013-03-30 21:24:30 -0300

    Big update: encode sentences (not paragraphs)
    Requires nltk (nltk.org) to be installed. "english.pickle" for splitting sentences included in this repo.

--- wikiBEAGLE.py
+++ wikiBEAGLE.py
@@ -19,6 +19,15 @@ import os
 import shutil
 import tty
 import select
+import nltk.data
+import math
+
+
+########
+# Load the english sentence punkt data
+########
+
+tokenizer = nltk.data.load('file:./english.pickle')
 
 
 ########
@@ -38,28 +47,28 @@ else:
 class NotTTYException(Exception): pass
 
 class TerminalFile:
-    def __init__(self,infile):
-        if not infile.isatty():
-            raise NotTTYException()
-        self.file=infile
+	def __init__(self,infile):
+		if not infile.isatty():
+			raise NotTTYException()
+		self.file=infile
 
-        #prepare for getch
-        self.save_attr=tty.tcgetattr(self.file)
-        newattr=self.save_attr[:]
-        newattr[3] &= ~tty.ECHO & ~tty.ICANON
-        tty.tcsetattr(self.file, tty.TCSANOW, newattr)
+		#prepare for getch
+		self.save_attr=tty.tcgetattr(self.file)
+		newattr=self.save_attr[:]
+		newattr[3] &= ~tty.ECHO & ~tty.ICANON
+		tty.tcsetattr(self.file, tty.TCSANOW, newattr)
 
-    def __del__(self):
-        #restoring stdin
-        import tty  #required this import here
-        tty.tcsetattr(self.file, tty.TCSADRAIN, self.save_attr)
+	def __del__(self):
+		#restoring stdin
+		import tty  #required this import here
+		tty.tcsetattr(self.file, tty.TCSADRAIN, self.save_attr)
 
-    def getch(self):
-        if select.select([self.file],[],[],0)[0]:
-            c=self.file.read(1)
-        else:
-            c=''
-        return c
+	def getch(self):
+		if select.select([self.file],[],[],0)[0]:
+			c=self.file.read(1)
+		else:
+			c=''
+		return c
 
 
 ########
@@ -86,34 +95,73 @@ def delHtml(text):
 	return(text)
 
 
-#define a function that cleans up a page of text from wikipedia, returning a list of paragraphs
+#define a function to delete anything inside parens
+def removeEncaps(text):
+	for encaps in [['(',')'],['[',']']]:
+		done = False
+		while not done:
+			if encaps[0] in text:
+				i = 0
+				while text[i]!=encaps[0]:
+					i += 1
+				if encaps[1] in text:
+					j = i
+					while text[j]!=encaps[1]:
+						j += 1
+					text = text[0:i]+text[(j+1):]
+				else:
+					done = True
+			else:
+				done = True
+	return(text)	
+
+
+#define a function that cleans up a page of text from wikipedia, returning a list of sentences
 def cleanPage(page):
 	pageLines = page.readlines()
 	pageLinesFinal = []
 	for thisPageLine in pageLines:
 		if thisPageLine[0:3]=='<p>':
 			line = delHtml(thisPageLine)
+			line = removeEncaps(line)
 			line = line.replace('\n','')
-			line = line.lower() #convert to lowercase
-			line = ''.join(re.findall('[a-z -]', line)) #keep only letters, spaces and dashes
+			line = line.lower()
 			line = line.replace('/',' ')
 			line = line.replace('citation needed',' ')
-			line = line.replace(' - ',' ')
-			line = line.strip()
-			while '  ' in line:
-				line = line.replace('  ',' ')
-			if line!='':
-				words = line.split(' ')
-				j = 0
-				while j<len(words):
-					temp = words[j].replace('-','')
-					if (len(temp)<(len(words[j])-1)) or ('http' in words[j]) or ('ftp' in words[j]) or ('linkback' in words[j]) or (len(words[j])>30) or (words[j]==''):
-						trash = words.pop(j)
-						del trash
-					else:
-						j = j+1
-				if len(words)>2:
-					pageLinesFinal.append(' '.join(words))
+			line = ''.join(re.findall('[a-z 0-9 ,;:.!?-]', line))
+			line = line.replace('mr.','mr')
+			line = line.replace('mrs.','mrs')
+			line = line.replace('ms.','ms')
+			line = line.replace('dr.','dr')
+			line = line.replace('ex.','ex')
+			line = line.replace('e.g.','eg')
+			line = line.replace('etc.','etc')
+			sentenceList = tokenizer.tokenize(line)
+			for line in sentenceList:
+				line = line.replace(',',' , ')
+				line = line.replace(';',' ; ')
+				line = line.replace(':',' : ')
+				line = line.replace('.',' . ')
+				line = line.replace('!',' ! ')
+				line = line.replace('?',' ? ')
+				line = line.replace(' - ',' ')
+				line = line.replace('-',' - ')
+				line = line.strip()
+				while '  ' in line:
+					line = line.replace('  ',' ')
+				if line!='':
+					words = line.split(' ')
+					j = 0
+					while j<len(words):
+						if ('http' in words[j]) or ('ftp' in words[j]) or ('linkback' in words[j]) or (len(words[j])>30) or (words[j]==''):
+							trash = words.pop(j)
+							del trash
+						else:
+							j = j+1
+					if len(words)>2:
+						pageLinesFinal.append('` '+' '.join(words))
+						if pageLinesFinal[-1][-1]=='.':
+							pageLinesFinal[-1] = pageLinesFinal[-1][:-1]+"'"
 	return pageLinesFinal
 
 
@@ -181,10 +229,11 @@ def getOpenNGrams(word, charVecList, charPlaceholder):
 ########
 
 numpy.random.seed(112358) #set the numpy random seed for (some) replicability
-chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '-']
+chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '`', "'", '-', ',', ';', ':', '.', '!', '?']
 charVecList = {}
 for char in chars:
 	charVecList[char] = normalize(numpy.random.randn(vectorLength) * vectorLength**-0.5)
+
 charPlaceholder = normalize(numpy.random.randn(vectorLength) * vectorLength**-0.5)
 wordPlaceholder = normalize(numpy.random.randn(vectorLength) * vectorLength**-0.5)
 perm1 = numpy.random.permutation(vectorLength)
@@ -262,15 +311,17 @@ def learnerLoop(freqList,coreNum,queueToLearner,queueFromLearner):
 				tmp.close()				
 				queueFromLearner.put('done')
 				sys.exit()
-			uniqueWords = {}
+			thisFreqList = {}
+			uniqueWords = []
 			words = line.split(' ')
 			queueFromLearner.put(['tokens',len(words)])
 			for word in words:
-				if word not in uniqueWords:
-					uniqueWords[word] = 1
+				if word not in thisFreqList:
+					thisFreqList[word] = 1
+					uniqueWords.append(word)
 				else:
-					uniqueWords[word] += 1
-			for word in uniqueWords:
+					thisFreqList[word] += 1
+			for word in thisFreqList:
 				if (not (word in indexList)): #word is new to this learner
 					newSize = len(indexList)+1
 					if newSize==1: #first new word, initialize lists
@@ -286,18 +337,24 @@ def learnerLoop(freqList,coreNum,queueToLearner,queueFromLearner):
 					formList[newSize-1] = normalize(numpy.add.reduce([seqOrdConv(ngram,perm1,perm2) for ngram in getOpenNGrams(word, charVecList, charPlaceholder)]))
 					indexList[word] = newSize-1
 				if word in freqList:
-					freqList[word] += uniqueWords[word]
+					freqList[word] += thisFreqList[word]
 				else:
-					freqList[word] = uniqueWords[word]
-			#perform encoding
+					freqList[word] = thisFreqList[word]
+			#compute sum number of tokens for later scaling of raw word frequencies
+			freqs = [v for k,v in freqList.items()]
+			maxFreq = max(freqs)*1.0
+			#perform context encoding
+			for j in range(len(uniqueWords)):
+				word = uniqueWords[j]
+				index = indexList[word]
+				#context encoding
+				for k in range(len(uniqueWords)):
+					if j!=k:
+						contextList[index] = contextList[index] + formList[indexList[uniqueWords[k]]]*(math.log(freqList[uniqueWords[k]]/maxFreq,2)/math.log(1.0/maxFreq,2))#(((2**(-(freqList[uniqueWords[k]]/maxFreq)))-.5)*2) #weighting contribution by inverse frequency
+			#perform order encoding
 			for j in range(len(words)):
 				word = words[j]
 				index = indexList[word]
-				#context encoding
-				for k in range(len(words)):
-					if j!=k:
-						contextList[index] = contextList[index] + formList[indexList[words[k]]]/freqList[words[k]] #weighting contribution by inverse frequency
-				#order encoding
 				for order in [1,2,3,4,5,6,7]: #only encode up to 7-grams
 					order = order + 1
 					for k in range(order):
@@ -307,11 +364,11 @@ def learnerLoop(freqList,coreNum,queueToLearner,queueFromLearner):
 								forms = [formList[indexList[wordTmp]] for wordTmp in wordsTmp]
 								forms[k] = wordPlaceholder
 								orderList[index] += seqOrdConv(forms,perm1,perm2)
-			#done a paragraph
+			#done a sentence
 			del(forms) #so that we can resize formList later
 			contextList.flush()
 			orderList.flush()
-			queueFromLearner.put('paragraph')
+			queueFromLearner.put('sentence')
 		queueFromLearner.put('page')
 
 
@@ -349,12 +406,12 @@ def startLearners():
 # Define a function to stop the learners
 ########
 
-def killAndCleanUp(pageNum, paragraphNum, tokenNum, timeTaken):
+def killAndCleanUp(pageNum, sentenceNum, tokenNum, timeTaken):
 	learners_alive = numCores
-	os.system('clear')
 	wordNum = len(freqList)
 	timeToPrint = str(datetime.timedelta(seconds=round(timeTaken)))
-	print 'wikiBEAGLE\n\nPages: '+str(pageNum)+'  Paragraphs: '+str(paragraphNum)+'  Words: '+str(wordNum)+'  Tokens: '+str(tokenNum)+'  Time: '+timeToPrint
+	os.system('clear')
+	print 'wikiBEAGLE\n\nPages: '+str(pageNum)+'  Sentences: '+str(sentenceNum)+'  Words: '+str(wordNum)+'  Tokens: '+str(tokenNum)+'  Time: '+timeToPrint
 	print '\nKilling learners... still alive: ' + str(learners_alive)
 	lastUpdateTime = time.time()
 	queueToLearner.put('die')
@@ -368,30 +425,30 @@ def killAndCleanUp(pageNum, paragraphNum, tokenNum, timeTaken):
 				learners_alive -= 1
 			elif message=='page':
 				pageNum += 1
-			elif message=='paragraph':
-				paragraphNum += 1
+			elif message=='sentence':
+				sentenceNum += 1
 			elif message[0]=='tokens':
 				tokenNum = tokenNum + message[1]
 		if (time.time()-lastUpdateTime)>1:
 			timeTaken += (time.time()-lastUpdateTime)
 			lastUpdateTime = time.time()
-			os.system('clear')
 			wordNum = len(freqList)
 			timeToPrint = str(datetime.timedelta(seconds=round(timeTaken)))
-			print 'wikiBEAGLE\n\nPages: '+str(pageNum)+'  Paragraphs: '+str(paragraphNum)+'  Words: '+str(wordNum)+'  Tokens: '+str(tokenNum)+'  Time: '+timeToPrint
+			os.system('clear')
+			print 'wikiBEAGLE\n\nPages: '+str(pageNum)+'  Sentences: '+str(sentenceNum)+'  Words: '+str(wordNum)+'  Tokens: '+str(tokenNum)+'  Time: '+timeToPrint
 			print '\nKilling learners... still alive: ' + str(learners_alive)
 	timeTaken += (time.time()-lastUpdateTime)
-	os.system('clear')
 	wordNum = len(freqList)
 	timeToPrint = str(datetime.timedelta(seconds=round(timeTaken)))
-	print 'wikiBEAGLE\n\nPages: '+str(pageNum)+'  Paragraphs: '+str(paragraphNum)+'  Words: '+str(wordNum)+'  Tokens: '+str(tokenNum)+'  Time: '+timeToPrint+'\n\n'
+	os.system('clear')
+	print 'wikiBEAGLE\n\nPages: '+str(pageNum)+'  Sentences: '+str(sentenceNum)+'  Words: '+str(wordNum)+'  Tokens: '+str(tokenNum)+'  Time: '+timeToPrint+'\n\n'
 	tmp = open('wikiBEAGLEdata/progress.txt','w')
-	tmp.write('\n'.join(map(str,[pageNum, paragraphNum, wordNum, tokenNum, int(round(timeTaken))])))
+	tmp.write('\n'.join(map(str,[pageNum, sentenceNum, wordNum, tokenNum, int(round(timeTaken))])))
 	tmp.close()
 	tmp = open('wikiBEAGLEdata/freqList','wb')
 	cPickle.dump(dict(freqList),tmp)
 	tmp.close()
-	return [pageNum,paragraphNum,tokenNum]
+	return [pageNum,sentenceNum,tokenNum]
 
 
 ########
@@ -400,19 +457,19 @@ def killAndCleanUp(pageNum, paragraphNum, tokenNum, timeTaken):
 
 if os.path.exists('wikiBEAGLEdata/progress.txt'):
 	tmp = open('wikiBEAGLEdata/progress.txt','r')
-	pageNum, paragraphNum, wordNum, tokenNum, timeTaken = map(int,tmp.readlines())
+	pageNum, sentenceNum, wordNum, tokenNum, timeTaken = map(int,tmp.readlines())
 	tmp.close()
 else:
 	pageNum = 0
-	paragraphNum = 0
+	sentenceNum = 0
 	tokenNum = 0
 	timeTaken = 0
 
 lastUpdateTime = time.time()
-os.system('clear')
 wordNum = len(freqList)
 timeToPrint = str(datetime.timedelta(seconds=round(timeTaken + (time.time()-lastUpdateTime))))
-print 'wikiBEAGLE\n\nPages: '+str(pageNum)+'  Paragraphs: '+str(paragraphNum)+'  Words: '+str(wordNum)+'  Tokens: '+str(tokenNum)+'  Time: '+timeToPrint+'\n\n(Press "q" to quit)'
+os.system('clear')
+print 'wikiBEAGLE\n\nPages: '+str(pageNum)+'  Sentences: '+str(sentenceNum)+'  Words: '+str(wordNum)+'  Tokens: '+str(tokenNum)+'  Time: '+timeToPrint+'\n\n(Press "q" to quit)'
 
 ########
 # Go!
@@ -421,7 +478,7 @@ stdin = TerminalFile(sys.stdin)
 startLearners()
 while True:
 	if stdin.getch()=='q':
-		killAndCleanUp(pageNum, paragraphNum, tokenNum, timeTaken)
+		killAndCleanUp(pageNum, sentenceNum, tokenNum, timeTaken)
 		sys.exit()
 	else:
 		if queueFromLearner.empty():
@@ -430,15 +487,15 @@ while True:
 			message = queueFromLearner.get()
 			if message=='page':
 				pageNum += 1
-			elif message=='paragraph':
-				paragraphNum += 1
+			elif message=='sentence':
+				sentenceNum += 1
 			elif message[0]=='tokens':
 				tokenNum = tokenNum + message[1]
 		if (time.time()-lastUpdateTime)>1:
 			timeTaken += (time.time()-lastUpdateTime)
 			lastUpdateTime = time.time()
-			os.system('clear')
 			wordNum = len(freqList)
 			timeToPrint = str(datetime.timedelta(seconds=round(timeTaken)))
-			print 'wikiBEAGLE\n\nPages: '+str(pageNum)+'  Paragraphs: '+str(paragraphNum)+'  Words: '+str(len(freqList))+'  Tokens: '+str(tokenNum)+'  Time: '+timeToPrint+'\n\n(Press "q" to quit)'
+			os.system('clear')
+			print 'wikiBEAGLE\n\nPages: '+str(pageNum)+'  Sentences: '+str(sentenceNum)+'  Words: '+str(len(freqList))+'  Tokens: '+str(tokenNum)+'  Time: '+timeToPrint+'\n\n(Press "q" to quit)'
 
